3e42dc8eba58e859bc300d9320dd0164ba47c383
SANJEEV <sa20095080@wipro.com>
2022-03-04 19:37:11 +0530

    added WebDriverManager

--- src/test/java/StepDefinitions/Base.java
+++ src/test/java/StepDefinitions/Base.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.sql.DriverManager;
 
 public class Base {
     public static WebDriver driver;
@@ -16,12 +18,14 @@ public class Base {
     public static WebDriver openBrowser(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+//                System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 System.out.println("Chrome Driver Invoked");
                 break;
             case "edge":
-                System.setProperty("webdriver.edge.driver", "C:\\msedgedriver.exe");
+//                System.setProperty("webdriver.edge.driver", "C:\\msedgedriver.exe");
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 System.out.println("Edge Driver Invoked");
                 break;
